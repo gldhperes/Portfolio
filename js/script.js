54220ef271cc827b13293fff806c01f5185a79b2
unknown <gldhperes@gmail.com>
2022-10-05 14:42:57 -0300

    Remodelagem de pagina, ainda falta ajeitar responsivo e outros

--- js/script.js
+++ js/script.js
@@ -1,15 +1,3 @@
-// let light = document.querySelector('.light')
-let body = document.querySelector('body')
-let projects = document.querySelector('.buttons')
-
-// light.onclick = function(){
-//     body.classList.toggle('active')
-// }
-
-// projects.onclick = function(){
-//     document.querySelector('#projects').classList.toggle('more')
-// }
-
 const inicial = document.querySelector('h3')
 function typeWriter(element){
     const textArray = element.innerHTML.split('')
@@ -24,13 +12,12 @@ function typeWriter(element){
 }   
 typeWriter(inicial)
 
-window.scrl = ScrollReveal({ reset:true});
+window.scrl = ScrollReveal({ reset:true });
 ScrollReveal({ distance: '20px' });
 //ScrollReveal de #home
 scrl.reveal('.apresentation', {duration: 3000});
-scrl.reveal('.buttonLinkedin', {duration: 9000})
-scrl.reveal('.buttonGitHub', {duration: 9000})
-scrl.reveal('.ilustracao', {duration: 3000})
+
+scrl.reveal('.macbook', {duration: 1000})
 //ScrollReveal de #about
 scrl.reveal('.programacao', {duration : 4000, origin: 'right'})
 scrl.reveal('.contentAbout', {duration : 2000, origin: 'left'})
